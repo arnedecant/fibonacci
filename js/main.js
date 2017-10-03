13138ef02196f698c5d06bd2636d53d35441b0ef
Arne Decant <arne@ris.be>
2017-10-03 14:34:19 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="jquery.js" />
+
+$(document).ready(function () {
+    var btn = $("#generate");
+    var result = $("#result");
+
+    btn.click(function () {
+        var eind = $("#getal").val();
+        result.append("<ul>");
+
+        var first = 0;
+        var second = 1; 
+        var current = 0 ;
+
+        for (var i = 0; i < eind; i++) {
+            result.append("<li>" + current + "</li>");
+
+            current = first + second;
+            second = first;
+            first = current;
+        }
+        result.append("</ul>");
+    });
+
+});
